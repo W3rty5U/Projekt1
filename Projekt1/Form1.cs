c261725ae7333e386e136e6c1d1d6b1c4257bd3a
Filip Majchrzak <filipm.gda@gmail.com>
2021-09-14 15:46:57 +0200

    experimental button speed implementation

--- Projekt1/Form1.cs
+++ Projekt1/Form1.cs
@@ -12,27 +12,38 @@ namespace Projekt1
 {
     public partial class Form1 : Form
     {
+        int points = 0;
+        double buttonX, buttonY;
         public Form1()
         {
             InitializeComponent();
+            buttonX = (button1.Left + button1.Right) / 2;
+            buttonY = (button1.Top + button1.Bottom) / 2;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             Point p = this.PointToClient(Cursor.Position);
-            int x = (button1.Left + button1.Right) / 2;
-            int y = (button1.Top + button1.Bottom) / 2;
-            if (p.X < x && this.ClientSize.Width > button1.Right)
-                button1.Left++;
-            else if (p.X > x && button1.Left > 0)
-                button1.Left--;
-            if (p.Y < y && this.ClientSize.Height > button1.Bottom)
-                button1.Top++;
-            else if (p.Y > y && button1.Top > 0)
-                button1.Top--;
+
+            double speedX, speedY;
+            speedX = Math.Abs(buttonX - p.X);
+            speedX = 1 - speedX / this.ClientSize.Width;
+            speedY = Math.Abs(buttonY - p.Y);
+            speedX = 1 - speedY / this.ClientSize.Height;
+
+            if (p.X < buttonX && this.ClientSize.Width > button1.Right)
+                buttonX += speedX * 0.05;
+            else if (p.X > buttonX && button1.Left > 0)
+                buttonX -= speedX * 0.05;
+            if (p.Y < buttonY && this.ClientSize.Height > button1.Bottom)
+                buttonY += speedY * 0.05;
+            else if (p.Y > buttonY && button1.Top > 0)
+                buttonY -= speedY * 0.05;
+
+            button1.Left = (int)buttonX;
+            button1.Top = (int)buttonY;
         }
 
-        int points = 0;
         private void button1_MouseClick(object sender, MouseEventArgs e)
         {
             points++;
