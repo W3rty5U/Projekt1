966166f0e294de1adc07b2977b068541db90aa55
Filip Majchrzak <filipm.gda@gmail.com>
2021-09-14 16:04:34 +0200

    "stable" variable button speed :flushed:

--- Projekt1/Form1.cs
+++ Projekt1/Form1.cs
@@ -17,31 +17,36 @@ namespace Projekt1
         public Form1()
         {
             InitializeComponent();
-            buttonX = (button1.Left + button1.Right) / 2;
-            buttonY = (button1.Top + button1.Bottom) / 2;
+            buttonX = button1.Left;
+            buttonY = button1.Top;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             Point p = this.PointToClient(Cursor.Position);
 
+            int buttonCenterX = (button1.Left + button1.Right) / 2;
+            int buttonCenterY = (button1.Top + button1.Bottom) / 2;
+
             double speedX, speedY;
             speedX = Math.Abs(buttonX - p.X);
             speedX = 1 - speedX / this.ClientSize.Width;
+            speedX *= 2;
             speedY = Math.Abs(buttonY - p.Y);
-            speedX = 1 - speedY / this.ClientSize.Height;
+            speedY = 1 - speedY / this.ClientSize.Height;
+            speedY *= 2;
 
-            if (p.X < buttonX && this.ClientSize.Width > button1.Right)
-                buttonX += speedX * 0.05;
-            else if (p.X > buttonX && button1.Left > 0)
-                buttonX -= speedX * 0.05;
-            if (p.Y < buttonY && this.ClientSize.Height > button1.Bottom)
-                buttonY += speedY * 0.05;
-            else if (p.Y > buttonY && button1.Top > 0)
-                buttonY -= speedY * 0.05;
+            if (p.X < buttonCenterX && this.ClientSize.Width > button1.Right)
+                buttonX += speedX;
+            else if (p.X > buttonCenterX && button1.Left > 0)
+                buttonX -= speedX;
+            if (p.Y < buttonCenterY && this.ClientSize.Height > button1.Bottom)
+                buttonY += speedY;
+            else if (p.Y > buttonCenterY && button1.Top > 0)
+                buttonY -= speedY;
 
-            button1.Left = (int)buttonX;
-            button1.Top = (int)buttonY;
+            button1.Left = (int)(buttonX );
+            button1.Top = (int)(buttonY );
         }
 
         private void button1_MouseClick(object sender, MouseEventArgs e)
